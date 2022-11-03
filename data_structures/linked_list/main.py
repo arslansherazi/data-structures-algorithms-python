84d1a09973024f7e45505834893c2c7e28ef969d
Arslan <arslanhaider.dev@hotmail.com>
2022-11-04 00:21:52 +0500

    separate single linked list main func

--- data_structures/linked_list/main.py
+++ data_structures/linked_list/main.py
@@ -4,36 +4,7 @@ from data_structures.linked_list.single_linked_list import LinkedList, Node
 if __name__ == '__main__':
     # Single Linked List
     print('#################### SINGLE LINKED LIST ####################')
-    linked_list = LinkedList()
 
-    node1 = Node(1)
-    node2 = Node(2)
-    node3 = Node(3)
-
-    linked_list.head = node1
-    node1.next = node2
-    node2.next = node3
-
-    linked_list.display()
-
-    linked_list.push(10)
-    linked_list.push(20)
-
-    linked_list.display()
-
-    linked_list.insert(100, 200)
-    linked_list.insert(2, 21)
-    linked_list.display()
-
-    linked_list.delete(200)
-    linked_list.delete(21)
-    linked_list.display()
-
-    linked_list.append(89)
-    linked_list.append(91)
-    linked_list.display()
-
-    linked_list.search(89)
 
     # Double Linked List
     print('#################### DOUBLE LINKED LIST ####################')
--- data_structures/linked_list/single_linked_list.py
+++ data_structures/linked_list/single_linked_list.py
@@ -77,3 +77,36 @@ class LinkedList:
             print(temp_node.data, end='\t')
             temp_node = temp_node.next
         print()
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+
+    node1 = Node(1)
+    node2 = Node(2)
+    node3 = Node(3)
+
+    linked_list.head = node1
+    node1.next = node2
+    node2.next = node3
+
+    linked_list.display()
+
+    linked_list.push(10)
+    linked_list.push(20)
+
+    linked_list.display()
+
+    linked_list.insert(100, 200)
+    linked_list.insert(2, 21)
+    linked_list.display()
+
+    linked_list.delete(200)
+    linked_list.delete(21)
+    linked_list.display()
+
+    linked_list.append(89)
+    linked_list.append(91)
+    linked_list.display()
+
+    linked_list.search(89)
