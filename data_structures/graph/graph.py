5ff9a5fcc11a26537222ba07e5f09cb8ca1e6d46
Arslan <arslanhaider.dev@hotmail.com>
2022-11-27 00:44:57 +0500

    graph data structure

---
+++ data_structures/graph/graph.py
@@ -0,0 +1,29 @@
+class Graph(object):
+    def __init__(self):
+        self.vertices = []
+        self.edges = []
+
+    def add_vertex(self, vertex):
+        pass
+
+    def add_edge(self, staring_vertex, ending_vertex):
+        pass
+
+    def display_paths(self, staring_vertex, ending_vertex):
+        pass
+
+    def delete_edge(self, staring_vertex, ending_vertex):
+        pass
+
+    def delete_vertex(self, vertex):
+        pass
+
+    def depth_first_traversal(self):
+        pass
+
+    def breadth_first_traversal(self):
+        pass
+
+
+if __name__ == '__main__':
+    pass
