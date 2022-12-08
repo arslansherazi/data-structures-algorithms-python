8774b690c53d6fd8204a5c343e4a202f725cf1ea
Arslan <arslanhaider.dev@hotmail.com>
2022-12-08 20:35:25 +0500

    add dijkstra algo basic structure

---
+++ data_structures/graph/algorithms/dijkstra_shortest_path.py
@@ -0,0 +1,10 @@
+class DijkstraShortestPath(object):
+    def __init__(self, graph):
+        self.graph = graph
+
+    def get_shortest_path(self) -> (str, int):
+        pass
+
+
+if __name__ == '__main__':
+    pass
