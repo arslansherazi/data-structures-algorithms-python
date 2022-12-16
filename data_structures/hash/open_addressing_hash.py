35977e1dc750e6f6e83aa710960458392f725409
Arslan <arslanhaider.dev@hotmail.com>
2022-12-16 20:24:02 +0500

    open addressing hashing structure

--- data_structures/hash/open_addressing_hash.py
+++ data_structures/hash/open_addressing_hash.py
@@ -7,3 +7,34 @@ class Node:
 class OpenAddressingHash:
     def __init__(self):
         self.__hash_table = []
+
+    def __hash_function(self, key):
+        pass
+
+    def __linear_probing(self, key):
+        pass
+
+    def __quadratic_probing(self, key):
+        pass
+
+    def __rehashing(self, key):
+        pass
+
+    def insert(self, key, value):
+        pass
+
+    def update(self, key, value):
+        pass
+
+    def delete(self, key):
+        pass
+
+    def get_value(self, key):
+        pass
+
+    @staticmethod
+    def search_key(hash_table_value: list, key) -> int:
+        pass
+
+    def display(self):
+        pass
