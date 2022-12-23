fc129d626c191e9d48ccde9e94625137d48cbf66
Arslan <arslanhaider.dev@hotmail.com>
2022-12-23 19:18:47 +0500

    shell sort

---
+++ algorithms/sorting/shell_sort.py
@@ -0,0 +1,29 @@
+""" Complexity --> O(n)  """
+
+from algorithms.calculate_time import calculate_time
+
+
+@calculate_time
+def shell_sort(_data):
+    data_length = len(_data)
+    interval = data_length // 2
+
+    while interval > 0:
+        for i in range(interval, data_length):
+            temp = _data[i]
+            j = i
+
+            # comparing the values with intervals (gaps) and sort
+            while j >= interval and _data[j - interval] > temp:
+                _data[j] = _data[j - interval]  # _data[j] = right side, _data[j - interval] = left side
+                j -= interval
+            _data[j] = temp
+        interval //= 2
+
+    return _data
+
+
+if __name__ == '__main__':
+    data = [20, 1, 13, 6, -8, 12, 90, -1, 94, 34, 89, 92, 45, 5, 9, 12, 0, 34]
+    sorted_data = shell_sort(data)
+    print(sorted_data)
