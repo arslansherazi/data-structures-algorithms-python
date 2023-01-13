634b5fe2ad341c020158a51ebbf0fde0c204e071
Arslan <arslanhaider.dev@hotmail.com>
2023-01-14 01:28:38 +0500

    binary search tree

---
+++ data_structures/tree/binary_search_tree/binary_search_tree.py
@@ -0,0 +1,19 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.right_child = None
+        self.left_child = None
+
+
+class BinarySearchTree:
+    def insert(self, data):
+        pass
+
+    def update(self, data):
+        pass
+
+    def delete(self, data):
+        pass
+
+    def search(self, data):
+        pass
