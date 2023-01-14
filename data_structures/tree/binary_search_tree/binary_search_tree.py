eb668a8319b8f13b012ca19512bcceb83493b9b4
Arslan <arslanhaider.dev@hotmail.com>
2023-01-14 18:42:12 +0500

    binary search tree

--- data_structures/tree/binary_search_tree/binary_search_tree.py
+++ data_structures/tree/binary_search_tree/binary_search_tree.py
@@ -6,8 +6,24 @@ class Node:
 
 
 class BinarySearchTree:
-    def insert(self, data):
-        pass
+    def __init__(self):
+        self.__root = None
+        
+    def insert(self, node=None, data=None):
+        if not self.__root:
+            self.__root = Node(data)
+            return self.__root
+            
+        # handle root node
+        if not node:
+            return Node(data)
+
+        if data < node.data:
+            node.left_child = self.insert(node.left_child, data)
+        else:
+            node.right_child = self.insert(node.right_child, data)
+
+        return node
 
     def update(self, data):
         pass
@@ -18,5 +34,47 @@ class BinarySearchTree:
     def search(self, data):
         pass
 
-    def traversal(self):
-        pass
+    def inorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left_child)
+            print(str(node.data) + ' -> ', end='')
+            self.inorder_traversal(node.right_child)
+
+    def preorder_traversal(self, node):
+        if node:
+            print(str(node.data) + ' -> ', end='')
+            self.inorder_traversal(node.left_child)
+            self.inorder_traversal(node.right_child)
+
+    def postorder_traversal(self, node):
+        if node:
+            self.inorder_traversal(node.left_child)
+            self.inorder_traversal(node.right_child)
+            print(str(node.data) + ' -> ', end='')
+
+
+if __name__ == '__main__':
+    bst = BinarySearchTree()
+
+    # root node
+    root = bst.insert(data=12)
+
+    bst.insert(root, 9)
+    bst.insert(root, 8)
+    bst.insert(root, 2)
+    bst.insert(root, 6)
+    bst.insert(root, 10)
+    bst.insert(root, 13)
+    bst.insert(root, 4)
+    bst.insert(root, 16)
+    bst.insert(root, 17)
+
+    # tree traversal
+    print('Inorder Traversal:')
+    bst.inorder_traversal(root)
+
+    print('\nPreorder Traversal:')
+    bst.preorder_traversal(root)
+
+    print('\nPostorder Traversal:')
+    bst.postorder_traversal(root)
