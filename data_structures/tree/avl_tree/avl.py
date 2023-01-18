0a41d61089cf9b3819775305238cc7e6844f000d
Arslan <arslanhaider.dev@hotmail.com>
2023-01-18 18:18:03 +0500

    finalize avl insertion

--- data_structures/tree/avl_tree/avl.py
+++ data_structures/tree/avl_tree/avl.py
@@ -15,11 +15,6 @@ class AVLTree(BinarySearchTree):
         self.__root = None
 
     def insert(self, node=None, data=None):
-        # handle root node
-        if not self.__root:
-            self.__root = Node(data)
-            return self.__root
-
         if not node:
             return Node(data)
 
@@ -32,23 +27,27 @@ class AVLTree(BinarySearchTree):
         node.height = 1 + max(self.get_height(node.left_child), self.get_height(node.right_child))
         balancing_factor = self.get_balancing_factor(node)
 
-        # CASE-1 Left Rotation
-        if balancing_factor < -1 and data > node.right_child.data:
-            return self.left_rotate(node)
+        if balancing_factor > 1:
+            # CASE-1 Right Rotation
+            if data < node.left_child.data:
+                return self.right_rotate(node)
 
-        # CASE-2 Right Rotation
-        if balancing_factor > 1 and data < node.left_child.data:
-            return self.right_rotate(node)
+            # CASE-2 Left Right Rotation
+            else:
+                node.left_child = self.left_rotate(node.left)
+                return self.right_rotate(node)
 
-        # CASE-3 Left Right Rotation
-        if balancing_factor > 1 and data > node.left_child.data:
-            node.right_child = self.left_rotate(node)
-            return self.right_rotate(node)
+        if balancing_factor < -1:
+            # CASE-3 Left Rotation
+            if data > node.right_child.data:
+                return self.left_rotate(node)
 
-        # CASE-4 Right Left Rotation
-        if balancing_factor < -1 and data < node.right_child.data:
-            node.left_child = self.right_rotate(node)
-            return self.left_rotate(node)
+            # CASE-4 Right Left Rotation
+            else:
+                node.right_child = self.right_rotate(node.right)
+                return self.left_rotate(node)
+
+        return node
 
     def left_rotate(self, z):
         y = z.right_child
@@ -95,11 +94,9 @@ class AVLTree(BinarySearchTree):
 if __name__ == '__main__':
     avl_tree = AVLTree()
 
-    # root node
     root = avl_tree.insert(data=5)
-
-    avl_tree.insert(root, 7)
-    avl_tree.insert(root, 8)
+    root = avl_tree.insert(root, 7)
+    root = avl_tree.insert(root, 8)
 
     print("Pre-order Traversal::")
     avl_tree.preorder_traversal(root)
